bd1f746c4e28f66fcd34def75e77cb852ea103c3
Giuseppe A Savino <peppe94x@hotmail.it>
2022-12-12 17:20:26 +0100

    Added sounds in adventures page, minor fix in navs links, added credits.

--- src/js/javascript_3.js
+++ src/js/javascript_3.js
@@ -8,3 +8,26 @@ $(document).ready(function(){
     $('#loginModal').modal('toggle');
   });
 });
+
+// Scripts
+const paperSound = new Audio();
+paperSound.src = 'assets/sound/pen.mp3';
+document.getElementById('list-one-list').addEventListener("click", paperFunction);
+document.getElementById('list-two-list').addEventListener("click", paperFunction);
+document.getElementById('list-three-list').addEventListener("click", paperFunction);
+document.getElementById('list-four-list').addEventListener("click", paperFunction);
+    function paperFunction(){
+        paperSound.play();
+      }
+      
+// I create a variable equals to the audio I want to reproduce
+const greatPaper = new Audio();
+greatPaper.src = 'assets/sound/great-page-turns.mp3';
+
+// Converting the variable in array and then call the method for each, after that I can enstablish the function
+const maps = Array.from(document.getElementsByClassName('suonoImmagine'));
+      maps.forEach(suonoImmagine => {
+        suonoImmagine.addEventListener('click', function handleClick(event) {
+          greatPaper.play(), event;
+        });
+      });
